3ecc23a30b9dd5c6cab43418ad1e4dce02e0f0cc
zzk123 <753809163@qq.com>
2018-03-20 22:42:55 +0800

    建造者模式

---
+++ src/com/zzk/builderpattern/ActorController.java
@@ -0,0 +1,14 @@
+package com.zzk.builderpattern;
+
+public class ActorController {
+	public Actor construct(ActorBuilder ab) {
+		Actor actor;
+		ab.buildType();
+		ab.buildSex();
+		ab.buildFace();
+		ab.buildCostume();
+		ab.buildHairstyle();
+		actor = ab.createActor();
+		return actor;
+	}
+}
---
+++ src/com/zzk/builderpattern2/ActorBuilder.java
@@ -0,0 +1,24 @@
+package com.zzk.builderpattern2;
+
+import com.zzk.builderpattern.Actor;
+
+abstract class ActorBuilder {
+	
+	protected static Actor actor = new Actor();
+	
+	public abstract void buildType();
+	public abstract void buildSex();
+	public abstract void buildFace();
+	public abstract void buildCostume();
+	public abstract void buildHairstyle();
+	
+	//工厂方法，返回一个完整的游戏角色对象
+	public static Actor construct(ActorBuilder ab) {
+		ab.buildType();
+		ab.buildSex();
+		ab.buildFace();
+		ab.buildCostume();
+		ab.buildHairstyle();
+		return actor;
+	}
+}
